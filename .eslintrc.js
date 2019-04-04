d516ca84c557cc83cd6585708f7583c04d8d4343
William Willsey <wwillsey@medallia.com>
2019-04-04 09:48:19 -0700

    update p5

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "extends": ["airbnb", "plugin:import/errors", "plugin:import/warnings"],
+    "settings": {
+        'import/resolver': 'node',
+        'import/parsers':  {
+            'typescript-eslint-parser': [ '.ts', '.tsx' ]
+        }
+    },
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import",
+        "mocha"
+    ],
+    "rules": {
+        "max-len": ["error", 130],
+        "comma-dangle": ["error", "only-multiline"],
+        "camelcase": ["error", {"properties": "never"}],
+        "mocha/no-exclusive-tests": "error",
+        "object-curly-newline": "off",
+        "function-paren-newline": "off",
+        "prefer-destructuring": "off",
+        "indent": "off",
+        "implicit-arrow-linebreak": "off"
+    },
+    "globals": {
+        "logger": true
+    },
+    "env": {
+        "node": true,
+        "mocha": true
+      }
+};
