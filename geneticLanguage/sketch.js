393f9587aee7fb769155b7452c752bf2ba0f58fd
William Willsey <wwillsey@medallia.com>
2019-06-24 17:17:24 -0700

    working with grid

--- geneticLanguage/sketch.js
+++ geneticLanguage/sketch.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-use-before-define, class-methods-use-this, no-undef */
 let containers;
 
-let rows = 5;
-let cols = 5;
+let rows = 4;
+let cols = 4;
 
-let xResolution = 50;
-let yResolution = 50;
+let xResolution = 20;
+let yResolution = 20;
+
+const programLength = 30;
+const mutateBy = 2;
+
 
 const operators = [
   '+',
@@ -25,11 +29,9 @@ const symbols = [
   'x',
   'y',
   'distFromMiddle',
-  // 'time',
+  'time',
   'musicAmplitude',
 ];
-const programLength = 55;
-const mutateBy = 2;
 
 let geneticShader;
 let music;
@@ -89,13 +91,13 @@ function mousePressed() {
 
 function setup() {
   // shaders require WEBGL mode to work
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  // createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(1000, 1000, WEBGL);
   noStroke();
   frameRate(60);
 
-  const exp = genExp(25);
-
-  const compiled = compileExp(exp);
+  containers = Array.from({length: rows}, (v, row) => Array.from({length: cols}, (v2, col) => new Container(row, col)));
+  const compiled = generateShader(containers);
   print(compiled)
   geneticShader = createShader(geneticVert, compiled);
 
@@ -110,6 +112,7 @@ function draw() {
 
   shader(geneticShader);
 
+  geneticShader.setUniform('resolution', [width, height]);
   const time = sin(frameCount * 0.01);
   geneticShader.setUniform('time', time);
 
@@ -124,68 +127,57 @@ function draw() {
   // print(amplitudeMean);
   geneticShader.setUniform('musicAmplitude', pow(max(amp - amplitudeMean, 0), 2));
 
-  rect(0,0,1,1)
+  rect(0,0,width, height);
 }
 
 
-function compileExp(exp) {
-  let result = ''
-  let i = 0;
-  let maxi = 0;
-  const push = (val) => {
-    result += `    x_${i} = ${val};\n`;
-    i++;
-    maxi = max(maxi, i);
-  }
-  const pop = () => {
-    if (i == 0) {
-      return 'none'
-    } else {
-      i--;
-      return `x_${i}`
-    }
-  }
-
-  exp.forEach(val => {
-    switch (val) {
-      case '+': push(`${pop()} + ${pop()}`); break;
-      case '-': push(`${pop()} - ${pop()}`); break;
-      case 'sin': push(`sin(${pop()})`); break;
-      case 'cos': push(`cos(${pop()})`); break;
-      case '/': push(`${pop()} / ${pop()}`); break;
-      case '*': push(`${pop()} * ${pop()}`); break;
-      case 'dist': const [a,b,c,d] = [pop(), pop(), pop(), pop()]; push(`pow(${a} - ${c}, 2.0) + pow(${b} - ${d}, 2.0)`); break;
-      case 'dup': const x = pop(); push(x); push(x); break;
-      // case 'drop': pop(); break;
-      // case 'rotate': length > 0 && push(splice(0,1)[0]); break;
-      case 'pow': push(`pow(${pop()}, ${pop()})`); break;
-      case 'atan': push(`atan(${pop()}, ${pop()})`); break;
-      default:
-        // push(env[val] != undefined ? env[val] : val);
-        push(val)
-    }
-  });
-
+function generateShader(containers) {
+  const containerExpsCompiled = [];
+  containers.forEach(row => row.forEach(container => containerExpsCompiled.push(container.compileExp())));
 
   return `
-    precision highp float;
-    varying vec2 vTexCoord;
+  precision highp float;
+  varying vec2 vTexCoord;
 
-    uniform float time;
-    uniform float musicAmplitude;
+  uniform float time;
+  uniform float musicAmplitude;
 
-    void main() {
-      float none = 0.0;
-      float x = vTexCoord.x;
-      float y = vTexCoord.y;
-      x = x - mod(x, 1.0 / ${xResolution}.0);
-      y = y - mod(y, 1.0 / ${yResolution}.0);
-      float distFromMiddle = pow(x - 0.5, 2.0) + pow(y - 0.5, 2.0);
+  const vec2 resolution = vec2(${width}.0, ${height}.0);
 
-    ${Array.from({length: maxi}, (v, i) => `float x_${i}`).join(';\n')};
-      ${result}
-    gl_FragColor = vec4(${pop()}, ${pop()}, ${pop()}, 1.0);
-}`;
+  vec3 rgb(float r, float g, float b){
+    return vec3(r / 255.0, g / 255.0, b / 255.0);
+  }
+
+  // this is the function that draws our rect
+  // it works just like rect in p5 except that it takes an additional color parameter at the end
+  vec3 rect (float x, float y, float w, float h, vec3 color){
+    vec2 coord = gl_FragCoord.xy;
+    coord.y = resolution.y - coord.y;
+    float width = 1.0 -mix(0.0, 1.0, step(x + w, coord.x));
+    float xPos =  1.0 -mix(0.0, 1.0, step(x, coord.x));
+    float height = 1.0 - mix(0.0, 1.0, step(y + h, coord.y));
+    float yPos = 1.0 - mix(0.0, 1.0, step(y, coord.y));
+    vec3 col = rgb(color.r, color.g, color.b);
+    return  col * ((height - yPos) * (width - xPos));
+  }
+
+  void main() {
+    float none = 0.0;
+    float x;
+    float y;
+    float distFromMiddle;
+
+    float width = resolution.x / ${cols}.0;
+    float height = resolution.y / ${rows}.0;
+
+    vec3 col;
+    vec3 r;
+    vec3 scene = vec3(0.0, 0.0, 0.0);
+
+    ${containerExpsCompiled.join('\n')}
+
+    gl_FragColor = vec4(scene, 1.0);
+  }`;
 }
 
 function showExpHovered() {
@@ -335,13 +327,14 @@ function cull() {
 }
 
 
+let containerNum = 0;
 class Container {
-  constructor(w,h, row, col) {
-    this.canvas = createGraphics(w, h, WEBGL);
+  constructor(row, col) {
     this.selected = false;
     this.exp = genExp(programLength);
     this.row = row;
     this.col = col;
+    this.id = containerNum++;
   }
 
   render() {
@@ -368,5 +361,69 @@ class Container {
     } else {
       this.render();
     }
+  }
+
+
+  compileExp() {
+    let result = ''
+    let i = 0;
+    let maxi = 0;
+    const push = (val) => {
+      result += `    x_${this.id}_${i} = ${val};\n`;
+      i++;
+      maxi = max(maxi, i);
+    }
+    const pop = () => {
+      if (i == 0) {
+        return 'none'
+      } else {
+        i--;
+        return `x_${this.id}_${i}`
+      }
+    }
+
+    this.exp.forEach(val => {
+      switch (val) {
+        case '+': push(`${pop()} + ${pop()}`); break;
+        case '-': push(`${pop()} - ${pop()}`); break;
+        case 'sin': push(`sin(${pop()})`); break;
+        case 'cos': push(`cos(${pop()})`); break;
+        case '/': push(`${pop()} / ${pop()}`); break;
+        case '*': push(`${pop()} * ${pop()}`); break;
+        case 'dist': const [a,b,c,d] = [pop(), pop(), pop(), pop()]; push(`pow(${a} - ${c}, 2.0) + pow(${b} - ${d}, 2.0)`); break;
+        case 'dup': const x = pop(); push(x); push(x); break;
+        // case 'drop': pop(); break;
+        // case 'rotate': length > 0 && push(splice(0,1)[0]); break;
+        case 'pow': push(`pow(${pop()}, ${pop()})`); break;
+        case 'atan': push(`atan(${pop()}, ${pop()})`); break;
+        default:
+          // push(env[val] != undefined ? env[val] : val);
+          push(val)
+      }
+    });
+
+    const blockWidth = width / cols * 2;
+    const blockHeight = height / rows * 2;
+    return `
+    x = gl_FragCoord.x;
+    y = gl_FragCoord.y;
+
+    x = mod(x, ${(blockWidth).toFixed(10)});
+    y = mod(y, ${(blockHeight).toFixed(10)});
+
+    x = x - mod(x, ${(blockWidth / xResolution).toFixed(10)});
+    y = y - mod(y, ${(blockHeight / yResolution).toFixed(10)});
+    x /= ${(blockWidth).toFixed(10)};
+    y /= ${(blockHeight).toFixed(10)};
+    distFromMiddle = pow(x - 0.5, 2.0) + pow(y - 0.5, 2.0);
+
+    ${Array.from({length: maxi}, (v, i) => `float x_${this.id}_${i}`).join(';\n')};
+    ${result}
+
+    col = vec3(${pop()} * 255.0, ${pop()} * 255.0, ${pop()} * 255.0);
+    // col = vec3(x, y, 0.0);
+    r = rect(${(this.col * width / cols*2).toFixed(10)}, ${(this.row * height / rows * 2 - height).toFixed(10)}, ${(width / cols * 2).toFixed(10)}, ${(height / rows * 2).toFixed(10)}, col);
+    scene = mix(scene, r, r);
+    `;
   }
 }
